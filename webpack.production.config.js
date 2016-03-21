a121cf9270b17afb01990a536f877900c66895c1
Dan Halverson <daniel.m.halverson@gmail.com>
2016-03-21 17:40:25 -0500

    updated stock quote to use a table instead.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -8,69 +8,65 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var StatsPlugin = require('stats-webpack-plugin');
 
 module.exports = {
-    entry: [
-        path.join(__dirname, 'app/main.js')
-    ],
-    output: {
-        path: path.join(__dirname, '/dist/'),
-        filename: '[name]-[hash].min.js'
-    },
-    plugins: [
-        new CopyWebpackPlugin([{
-            from: 'app/images/',
-            to: 'images/'
-        }, {
-            from: 'app/extras'
-        }]),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'app/index.tpl.html',
-            inject: 'body',
-            favicon: 'app/favicon.ico',
-            filename: 'index.html'
-        }),
-        new ExtractTextPlugin('[name]-[hash].min.css'),
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                warnings: false,
-                screw_ie8: true
-            }
-        }),
-        new StatsPlugin('webpack.stats.json', {
-            source: false,
-            modules: false
-        }),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        }),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        })
-    ],
-    module: {
-        loaders: [{
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            loader: 'babel'
-        }, {
-            test: /\.json?$/,
-            loader: 'json'
-        }, {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss')
-        }, {
-            test: /\.(ttf|eot|woff2|svg|png|woff)$/,
-            loader: 'file-loader?name=assets/[name].[ext]'
-        }, {
-            test: /\.php$/,
-            loader: 'file-loader?name=[name].[ext]'
-        }, {
-            test: /\.html$/,
-            loader: 'raw'
-        }]
-    },
-    postcss: [
-        require('autoprefixer')
-    ]
+  entry: [
+    path.join(__dirname, 'app/main.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name]-[hash].min.js'
+  },
+  plugins: [
+    new CopyWebpackPlugin([{
+      from: 'app/images/',
+      to: 'images/'
+    }, {
+      from: 'app/extras'
+    }]),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'app/index.tpl.html',
+      inject: 'body',
+      favicon: 'app/favicon.ico',
+      filename: 'index.html'
+    }),
+    new ExtractTextPlugin('[name]-[hash].min.css'),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }, {
+      test: /\.json?$/,
+      loader: 'json'
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('style', 'css')
+    }, {
+      test: /\.(ttf|eot|woff2|svg|png|woff)$/,
+      loader: 'file-loader?name=assets/[name].[ext]'
+    }, {
+      test: /\.php$/,
+      loader: 'file-loader?name=[name].[ext]'
+    }, {
+      test: /\.html$/,
+      loader: 'raw'
+    }]
+  },
+  postcss: [
+    require('autoprefixer')
+  ]
 };
