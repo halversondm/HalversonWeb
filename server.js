fcb198cbb4bb6566b4ea5ca67a2ec75e8396f22b
Dan Halverson <daniel.m.halverson@gmail.com>
2016-04-22 23:15:42 -0500

    remove unnecessary stuff

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /* eslint no-console: 0 */
 
-import path from 'path';
-import express from 'express';
-import webpack from 'webpack';
-import webpackMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from './webpack.config.js';
-import bodyParser from 'body-parser';
+var path = require( 'path');
+var express = require( 'express');
+var webpack = require( 'webpack');
+var webpackMiddleware = require( 'webpack-dev-middleware');
+var webpackHotMiddleware = require( 'webpack-hot-middleware');
+var config = require( './webpack.config.js');
+var bodyParser = require( 'body-parser');
 
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
